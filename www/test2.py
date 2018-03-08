337e681471c9becfba66d914db2b247ef9c36eb9
Ricky <macbook@macbookRickyPro.lan>
2018-03-08 08:23:43 +0800

    update gitignore file

---
+++ www/test2.py
@@ -0,0 +1,12 @@
+def foo(a, b = 10, *c, d,**kw):
+    pass
+
+import inspect
+
+sig = inspect.signature(foo)
+sig.parameters
+sig.parameters.items()
+sig.parameters.values()
+sig.parameters.keys()
+for name, param in sig.parameters.items():
+    print(name+' type=',param.kind)
